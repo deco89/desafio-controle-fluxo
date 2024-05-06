47711875a24b2b6eac49f718ea01daec00f3a330
Andre Fonseca <decobf15@gmail.com>
2024-05-06 18:12:54 -0300

    first commit

---
+++ src/Contador.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro");
+        int parametroUm = sc.nextInt();
+        System.out.println("Digite o segundo parâmetro");
+        int parametroDois = sc.nextInt();
+
+        try {
+            if (parametroDois <= parametroUm) {
+                throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+            }
+            contar(parametroUm, parametroDois);
+
+        } catch (ParametrosInvalidosException exception) {
+            System.out.println(exception.getMessage());
+        }
+
+        sc.close();
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        int contagem = parametroDois - parametroUm;
+        if (contagem < 0) {
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+        }
+
+        for (int i = 1; i <= contagem; i++) {
+            System.out.println("Imprimindo o número " + i);
+        }
+    }
+}
