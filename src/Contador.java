21b36d15d533eec9e4d10dcf4758b32f47357579
Andre Fonseca <decobf15@gmail.com>
2024-05-06 18:17:24 -0300

    código corrigido

--- src/Contador.java
+++ src/Contador.java
@@ -23,9 +23,6 @@ public class Contador {
 
     static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
         int contagem = parametroDois - parametroUm;
-        if (contagem < 0) {
-            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
-        }
 
         for (int i = 1; i <= contagem; i++) {
             System.out.println("Imprimindo o número " + i);
